ed5681dcd7591d3e118e120ccb45af314727e01c
Cristi Badeu <cristi.badeu@gmail.com>
2022-06-14 13:12:31 +0300

    Added components refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import './homepage.styles.scss';
-import HomePage from './homepage.component';
+
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
